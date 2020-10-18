810df974e549a8cd6eb367344ba2d18c4964b4dc
Gustavo Bodra <gustavobodra@yahoo.com.br>
2020-10-18 11:08:13 -0300

    Comparacao com mq

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ import matplotlib.pyplot as plt
 
 
 def load_file(filename):
+    """
+    Carrega o dataset para a memória
+        :param filename: nome do arquivo
+
+        :return: matriz x e y
+    """
     x_input = []
     y_input = []
     f = open("data/" + filename + ".txt")
@@ -25,19 +31,43 @@ def load_file(filename):
 
 
 def subtract_mean(x, y):
-    x_mean = sum(x) / len(x)
+    """
+    Subtrair a média de todo o dataset
+        :param x: entrada x
+        :param y: entrada y
+
+        :return: matriz ajustada
+    """
+    if isinstance(x[0], list):
+        x_zipped = list(zip(*x))
+        x1_mean = sum(x_zipped[0]) / len(x_zipped[0])
+        x2_mean = sum(x_zipped[1]) / len(x_zipped[1])
+    else:
+        x_mean = sum(x) / len(x)
+
     y_mean = sum(y) / len(y)
     x_adjusted = []
     y_adjusted = []
 
     for i in range(len(x)):
-        x_adjusted.append(x[i] - x_mean)
+        if isinstance(x[0], list):
+            x_adjusted.append([x[i][0] - x1_mean, x[i][1] - x2_mean])
+        else:
+            x_adjusted.append(x[i] - x_mean)
+
         y_adjusted.append(y[i] - y_mean)
 
     return x_adjusted, y_adjusted
 
 
 def covariance(a, b):
+    """
+    Calcula a covariancia entre dois pontos
+        :param a: ponto a
+        :param b: ponto b
+
+        :return: covariancia entre a e b
+    """
     covariance_result = 0
 
     for i in range(len(a)):
@@ -47,91 +77,40 @@ def covariance(a, b):
 
 
 def covariance_matrix(x, y):
+    """
+    Calcula matriz de covariancia
+        :param x: entrada x
+        :param y: entrada y
+
+        :return: matriz de covariancia
+    """
     cov_matrix = []
-    cov_x_x = covariance(x, x)
-    cov_x_y = covariance(x, y)
-    cov_matrix.append([cov_x_x, cov_x_y])
-    cov_y_x = covariance(y, x)
-    cov_y_y = covariance(y, y)
-    cov_matrix.append([cov_y_x, cov_y_y])
+
+    if not isinstance(x[0], list):
+        cov_x_x = covariance(x, x)
+        cov_x_y = covariance(x, y)
+        cov_matrix.append([cov_x_x, cov_x_y])
+        cov_y_x = covariance(y, x)
+        cov_y_y = covariance(y, y)
+        cov_matrix.append([cov_y_x, cov_y_y])
+    else:
+        x_zipped = list(zip(*x))
+        x1 = x_zipped[0]
+        x2 = x_zipped[1]
+        cov_x_x = covariance(x1, x1)
+        cov_x_y = covariance(x1, y)
+        cov_x_x2 = covariance(x1, x2)
+        cov_matrix.append([cov_x_x, cov_x_y, cov_x_x2])
+        cov_y_x = covariance(y, x1)
+        cov_y_y = covariance(y, y)
+        cov_y_x2 = covariance(y, x2)
+        cov_matrix.append([cov_y_x, cov_y_y, cov_y_x2])
+        cov_x2_x = covariance(x2, x1)
+        cov_x2_y = covariance(x2, y)
+        cov_x2_x2 = covariance(x2, x2)
+        cov_matrix.append([cov_x2_x, cov_x2_y, cov_x2_x2])
 
     return cov_matrix
-
-
-def matrix_empty(rows, cols):
-    """
-    Cria uma matriz vazia
-        :param rows: número de linhas da matriz
-        :param cols: número de colunas da matriz
-
-        :return: matriz preenchida com 0.0
-    """
-    M = []
-    while len(M) < rows:
-        M.append([])
-        while len(M[-1]) < cols:
-            M[-1].append(0.0)
-
-    return M
-
-
-def matrix_transpose(m):
-    """
-    Retorna a matriz transposta
-        :param m: matriz a ser transposta
-
-        :return: resultado da matriz de entrada transposta
-    """
-    if not isinstance(m[0], list):
-        m = [m]
-
-    rows = len(m)
-    cols = len(m[0])
-
-    mt = matrix_empty(cols, rows)
-
-    for i in range(rows):
-        for j in range(cols):
-            mt[j][i] = m[i][j]
-
-    return mt
-
-
-def matrix_multiply(a, b):
-    """
-    Retorna o produto da multiplicação da matriz a com b
-        :param a: primeira matriz
-        :param b: segunda matriz
-
-        :return: matriz resultante
-    """
-    rows_a = len(a)
-    cols_a = len(a[0])
-    rows_b = len(b)
-    cols_b = len(b[0])
-    if cols_a != rows_b:
-        raise ArithmeticError('O número de colunas da matriz a deve ser igual ao número de linhas da matriz b.')
-
-    result_matrix = matrix_empty(rows_a, cols_b)
-    for i in range(rows_a):
-        for j in range(cols_b):
-            total = 0
-            for ii in range(cols_a):
-                total += a[i][ii] * b[ii][j]
-            result_matrix[i][j] = total
-
-    return result_matrix
-
-
-def vector_to_matrix(vector):
-    if isinstance(vector[0], list):
-        raise ArithmeticError('Não é um vetor')
-
-    matrix = matrix_empty(len(vector), 1)
-    for i in range(len(vector)):
-        matrix[i][0] = vector[i]
-
-    return matrix
 
 
 def calc_pca(dataset):
@@ -162,16 +141,6 @@ def calc_pca(dataset):
     gradient_2 = feature_vector_2[1] / feature_vector_2[0]
     logging.info(feature_vector)
 
-    logging.info("*******VALORES FINAIS*******")
-    # feature_vector = vector_to_matrix(feature_vector)
-    # feature_vector_t = matrix_transpose(feature_vector)
-    # input_x_y = [list(a) for a in zip(x_minus_mean, y_minus_mean)]
-    # input_t = matrix_transpose(input_x_y)
-    # logging.info(feature_vector_t)
-    # logging.info(input_t)
-    # result = matrix_multiply(feature_vector_t, input_t)
-    # logging.info(result)
-
     logging.info("*******PLOT*******")
     plt.plot(x_minus_mean, y_minus_mean, '+', color='black', label='Dados ajustados')
     plt.plot(0, 0, 'x', color='blue', label='Média')
@@ -185,18 +154,24 @@ def calc_pca(dataset):
     pca_2_y = [min_x * gradient_2, 0, max_x * gradient_2]
     plt.plot(pca_2_x, pca_2_y, color='orange', label='Componente secundário')
 
+    if dataset == "US_Census":
+        x_mq = 2010 - (sum(x) / len(x))
+        y_mq = 286.9128 - (sum(y) / len(y))
+        plt.plot([x_minus_mean[0], x_mq], [y_minus_mean[0], y_mq], color='purple', label='Mínimos Quadrados')
+
+    if dataset == "alpswater":
+        x_mq = 31.06 - (sum(x) / len(x))
+        y_mq = 214.3658 - (sum(y) / len(y))
+        plt.plot([x_minus_mean[0], x_mq], [y_minus_mean[0], y_mq], color='purple', label='Mínimos Quadrados')
+
     plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05))
     plt.tight_layout()
     plt.title("Dataset: " + dataset)
     plt.show()
-    # plt.savefig("./images/" + dataset + ".png")
 
 
 logging.basicConfig(stream=sys.stderr, level=logging.INFO)
-calc_pca("dummy")
 
 calc_pca("alpswater")
 
 calc_pca("US_Census")
-
-calc_pca("economic_growth")
